d18dbdb7172c4afbcb6281626f0670c4a4c554ef
Marc Gauthier <mar.gauthier@cgi.com>
2017-06-16 13:21:53 -0400

    Ajout Apache

--- database/scripts/1-create-tables.sql
+++ database/scripts/1-create-tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE customers(
-  id CHAR (36) NOT NULL,
-  PRIMARY KEY(id),
-  prenom VARCHAR(64),
-  nom VARCHAR(64),
-  tel VARCHAR(64)
-);
+    username CHAR (8) NOT NULL,
+    PRIMARY KEY(username),
+    prenom VARCHAR(64),
+    nom VARCHAR(64),
+    tel VARCHAR(64)
+  );
 
-INSERT INTO customers ( id, prenom, nom, tel ) VALUES ( uuid(), 'Jane', 'Smith', '418-123-4567' );
-INSERT INTO customers ( id, prenom, nom, tel ) VALUES ( uuid(), 'Dave', 'Richards', '514-333-4444' );
+  INSERT INTO customers ( username, prenom, nom, tel ) VALUES ( 'sjane01', 'Jane', 'Smith', '418-123-4567' );
+  INSERT INTO customers ( username, prenom, nom, tel ) VALUES ( 'rdave01', 'Dave', 'Richards', '514-333-4444' );
--- deploy_environnement.sh
+++ deploy_environnement.sh
@@ -5,9 +5,14 @@
 
 docker-compose up -d
 
-#for sqlfile in `ls -1 database/scripts/*.sql|sort -n`; do
-#  docker-compose exec db sh -c "mysql -u ${ENV_DB_USER} -p${ENV_DB_PASSWORD} -D ${ENV_DB_NAME} < /tmp/`basename ${sqlfile}`"
-#done
+while ! docker-compose exec db sh -c "mysql -u ${ENV_DB_USER} -p${ENV_DB_PASSWORD} -D ${ENV_DB_NAME} -e 'show tables'" > /dev/null 2>&1; do 
+  echo "Database is not available"
+  sleep 1
+done
+
+for sqlfile in `ls -1 database/scripts/*.sql|sort -n`; do
+  docker-compose exec db sh -c "mysql -u ${ENV_DB_USER} -p${ENV_DB_PASSWORD} -D ${ENV_DB_NAME} < /tmp/`basename ${sqlfile}`"
+done
 
 cat ${ENV_APP_VOLUME}/META-INF/context.xml|sed -e "s/username=\".*\"/username=\"${ENV_DB_USER}\"/" -e "s/password=\".*\"/password=\"${ENV_DB_PASSWORD}\"/" -e "s#url=\"jdbc:mysql://.*:.*/.*\"#url=\"jdbc:mysql://mysql-server:3306/${ENV_DB_NAME}\"#" > ${ENV_APP_VOLUME}/META-INF/context.xml.new
 cp ${ENV_APP_VOLUME}/META-INF/context.xml ${ENV_APP_VOLUME}/META-INF/context.xml.ori
